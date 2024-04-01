41bfb5ff4cd6cda5a522120ac6301d3205313b8b
Kiko <kikojebest@gmail.com>
2024-04-01 15:28:35 +0200

    Pridany footer a header

---
+++ footer.php
@@ -0,0 +1,44 @@
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h5>O nás</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      </div>
+      <div class="col-md-4">
+        <h5>Naše sociálne siete:</h5>
+        <ul class="list-inline">
+          <a href="https://instagram.com/kikoszabo">
+          <ion-icon name="logo-instagram"></ion-icon>
+          </a>
+          <a href="https://facebook.com/kiko.szabo.1">
+          <ion-icon name="logo-facebook"></ion-icon>
+          </a>  
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <h5>Kontaktuj nás</h5>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="mailto:kikojebest@gmail.com" class="text-decoration-none">
+              <ion-icon name="mail"></ion-icon>
+          <li class="list-inline-item">
+            <a href="tel:+421 907 464 888" class="text-decoration-none">
+              <ion-icon name="call"></ion-icon>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a href="https://www.google.com/maps/place/Gábora+Bethlena+4892%2F47,+940+02+Nové+Zámky/@47.9791516,18.1610597,17z/data=!3m1!4b1!4m6!3m5!1s0x476b014b5e6df149:0xaccd6f57ebcf2bc0!8m2!3d47.979148!4d18.16364!16s%2Fg%2F11h209fww0?entry=ttu" class="text-decoration-none">
+              <ion-icon name="location"></ion-icon>
+            </a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <span class="text-muted">&copy; 2023 Kristián Szabó</span>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Môj blog</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  <header>
+  <nav class="navbar navbar-expand-lg m-3 fixed-top" id="main-navbar">
+      <div class="container-fluid py-3">
+          <a class="navbar-brand text-dark font-italic fw-bold fs-3 mx-3 mx-lg-5" href="index.html">
+              <img src="img/logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-top">
+              <span class="d-none d-lg-inline">GYM U.K.F</span>
+              <span class="d-inline d-lg-none logo-small">GymUKF</span>
+          </a>
+          <button class="navbar-toggler mx-3 mx-lg-5 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end mx-3 mx-lg-5" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                  <a class="nav-link fw-bold fs-5 mx-2" href="index.html">Home</a>
+                  <a class="nav-link fw-bold fs-5 mx-2" href="gallery.html">Gallery</a>
+                  <a class="nav-link fw-bold fs-5 mx-2" href="contact.html">Contact</a>
+                  <a class="nav-link fw-bold fs-5 mx-2" href="faq.html">FAQ</a>
+              </div>
+          </div>
+      </div>
+  </nav>
+</header> 
