87bcc78b182205d1fbe7b88c61c59f5b43620188
Kiko <kikojebest@gmail.com>
2024-04-24 22:17:31 +0200

    Mini Fix

--- blog.php
+++ blog.php
@@ -33,8 +33,8 @@ require_once 'header.php';
                         <p class="card-text"><?php echo $row['content']; ?></p>
                         <p class="card-text"><small class="text-muted">Od <?php echo $row['author']; ?> dňa <?php echo date('j. F Y', strtotime($row['created_at'])); ?></small></p>
                     <?php if ($_SESSION['user_id'] == $row['user_id'] || $_SESSION['role'] === 'admin') : ?>
-                        <a href="#deleteModal-<?php echo $row['id']; ?>" class="btn button-farba" data-toggle="modal">Delete</a>
-                        <a href="#editModal-<?php echo $row['id']; ?>" class="btn button-farba" data-toggle="modal">Edit</a>
+                        <a href="#deleteModal-<?php echo $row['id']; ?>" class="btn delete-button" data-toggle="modal">Zmazať</a>
+                        <a href="#editModal-<?php echo $row['id']; ?>" class="btn button-farba" data-toggle="modal">Upraviť</a>
                     <?php endif; ?>
                     </div>
                 </div>
@@ -44,19 +44,19 @@ require_once 'header.php';
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Delete Post</h5>
+                                <h5 class="modal-title" id="deleteModalLabel">Zmazať príspevok</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <form method="POST" action="blog.php">
                                 <div class="modal-body">
-                                    Are you sure you want to delete this post?
+                                    Naozaj chcete zmazať tento príspevok?
                                     <input type="hidden" name="post_id" value="<?php echo $row['id']; ?>">
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                    <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Zrušiť</button>
+                                    <button type="submit" name="delete" class="btn delete-button">Zmazať</button>
                                 </div>
                             </form>
                         </div>
@@ -68,7 +68,7 @@ require_once 'header.php';
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="editModalLabel">Edit Post</h5>
+                                <h5 class="modal-title" id="editModalLabel">Upraviť príspevok</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
@@ -76,22 +76,22 @@ require_once 'header.php';
                             <form method="POST" action="blog.php" enctype="multipart/form-data">
                                 <div class="modal-body">
                                     <div class="form-group">
-                                        <label for="title">Title</label>
+                                        <label for="title">Názov</label>
                                         <input type="text" class="form-control" id="title" name="title" value="<?php echo $row['title']; ?>">
                                     </div>
                                     <div class="form-group">
-                                        <label for="content">Content</label>
+                                        <label for="content">Obsah</label>
                                         <textarea class="form-control" id="content" name="content"><?php echo $row['content']; ?></textarea>
                                     </div>
                                     <div class="form-group">
-                                        <label for="image">Image</label>
+                                        <label for="image">Obrázok</label>
                                         <input type="file" class="form-control" id="image" name="image">
                                     </div>
                                     <input type="hidden" name="post_id" value="<?php echo $row['id']; ?>">
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                    <button type="submit" name="edit" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Zrušiť</button>
+                                    <button type="submit" name="edit" class="btn btn-primary">Uložiť zmeny</button>
                                 </div>
                             </form>
                         </div>
